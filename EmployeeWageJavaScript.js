8d60d17585a47cb0de78138d703dff28db794b0f
vish <vishgirase95@gmail.com>
2021-12-07 21:50:53 +0530

    [vishal] add function calling

--- EmployeeWageJavaScript.js
+++ EmployeeWageJavaScript.js
@@ -1,11 +1,38 @@
-const Emp_Absent=0;
-const Emp_Present=1;
-let Emp_AttenDance=Math.floor(Math.random()*2);
+const Emp_PartTime=0;
+const Emp_FullTime=1;
+const Emp_Absent=2;
+
+const PartTime_Hours=4;
+const FullTime_hours=8;
+const Absent_hours=0;
+const WagePerHr=20;
+let Emp_TodayWorkingHR=0;
+let DailyWage=0;
+
+let Emp_AttenDance=Math.floor(Math.random()*3);
 console.log(Emp_AttenDance);
-if (Emp_AttenDance==Emp_Present) {
+
+
+function Employe_DailyWage(Emp_AttenDance){
+
+    switch (Emp_AttenDance) {
+        case Emp_PartTime:
+            Emp_TodayWorkingHR=PartTime_Hours;
     
-    console.log("Employee is Present");
-} else {
-    console.log("Employee is Absent");
-    
+            break;
+        case Emp_FullTime:
+                Emp_TodayWorkingHR=FullTime_hours;
+            break;
+       case Emp_Absent:
+                    Emp_TodayWorkingHR=Absent_hours;
+            break;
+        default:
+            console.log("there is an Error");
+            break;
+    }
+return Emp_TodayWorkingHR;
 }
+
+Emp_TodayWorkingHR=Employe_DailyWage(Emp_AttenDance);
+DailyWage=Emp_TodayWorkingHR*WagePerHr;
+console.log("Emp Daily WAge: "+ DailyWage);
