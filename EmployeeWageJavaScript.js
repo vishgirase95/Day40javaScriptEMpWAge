1038f0da41e4113952c5c09f5e9b11ec8f95e486
vish <vishgirase95@gmail.com>
2021-12-08 16:42:23 +0530

    [vishal] added daily wage in array

--- EmployeeWageJavaScript.js
+++ EmployeeWageJavaScript.js
@@ -7,12 +7,12 @@ const FullTime_hours=8;
 const Absent_hours=0;
 const Total_workingHours=8;
 const WagePerHr=20;
+const TotalWorkDays=20;
 let Emp_TodayWorkingHR=0;
 let DailyWage=0;
 let monthlywage=0;
 let TotalHrs=0;
-
-
+let DailyWAgesArray = [];
 
 function Employe_DailyWage(Emp_AttenDance){
 
@@ -34,17 +34,21 @@ function Employe_DailyWage(Emp_AttenDance){
     return Emp_TodayWorkingHR;
 
 }
+function calcDailyWage(Emp_TodayWorkingHR){
+    return Emp_TodayWorkingHR*WagePerHr;
+}
+
 let Monthdays=0
-while(Monthdays<20 && TotalHrs<160 ){
+while(Monthdays<TotalWorkDays && TotalHrs<160 ){
     Emp_AttenDance=Math.floor(Math.random()*3);
     Emp_TodayWorkingHR=Employe_DailyWage(Emp_AttenDance);
-monthlywage+=Emp_TodayWorkingHR;
-TotalHrs+=Emp_TodayWorkingHR;
+    DailyWAgesArray.push(calcDailyWage(Emp_TodayWorkingHR));
+    TotalHrs += Emp_TodayWorkingHR;
 
+   
 Monthdays++;
 }
 
-
-
-DailyWage=monthlywage*WagePerHr;
-console.log("Emp Daily WAge: "+ DailyWage);
+let TotalEmpWage=calcDailyWage(TotalHrs);
+console.log(DailyWAgesArray);
+console.log("Total Days : "+TotalWorkDays  +"\nTotal Hrs : "+TotalHrs+"\nEmpWage : "+ TotalEmpWage);
