300a1a511dee5368989702ced267827969688fb3
vish <vishgirase95@gmail.com>
2021-12-10 22:59:01 +0530

    [vishal] added regex

--- EmpWageClass.js
+++ EmpWageClass.js
@@ -1,17 +1,34 @@
 class Employee{
-    constructor(id,name,salary){
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
-    }
 
+
+    constructor(...params){
+        this.id=params[0];
+        this.name=params[1];
+        this.salary=params[2];
+    }
+ get name(){return this._name;}
+ set name(name){
+let NameRegex=RegExp('^[A-Z]{1}[a-z]{3,}$');
+if(NameRegex.test(name))
+this._name=name;
+else throw 'The name is incorrect';
+
+ }
     string(){
         return "ID : "+this.id+"\nName : "+this.name+"\nSalary : "+ this.salary
     }
 }
-
-obj=new Employee(1,"vishal",200);
+obj=new Employee(1,"Vish",200);
 
 console.log(obj.string());
-obj.name="raj";
-console.log(obj.string());
+
+// console.log(obj.name);
+ 
+
+try {
+  obj.name="Raj";
+console.log(obj.string());  
+
+} catch (e) {
+    console.log(e);
+}
