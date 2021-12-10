f7f1d605713dfb0c27deadce65556fee9e47bd1e
vish <vishgirase95@gmail.com>
2021-12-10 19:48:07 +0530

    [vishal] added class extended

--- EmpWageClass.js
+++ EmpWageClass.js
@@ -1,16 +1,18 @@
 class Employee{
-    constructor(id,name,salary){
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
+    constructor(...params){
+        this.id=params[0];
+        this.name=params[1];
+        this.salary=params[2];
+        this.gender=params[3];
+        this.date=params[4];
     }
 
     string(){
-        return "ID : "+this.id+"\nName : "+this.name+"\nSalary : "+ this.salary
+        return "ID : "+this.id+"\nName : "+this.name+"\nSalary : "+ this.salary+"\nGender : "+this.gender+"\nDate : "+this.date
     }
 }
 
-obj=new Employee(1,"vishal",200);
+obj=new Employee(1,"vishal",200,"male","2/06/21");
 
 console.log(obj.string());
 obj.name="raj";
